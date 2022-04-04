69b2c378c0a95b22647721e3aca63ce5ffd284a0
Andrei Spinachi <andreeaspinaci@gmail.com>
2022-04-05 02:01:29 +0300

    Clean-up

--- src/main/java/com/hostelworld/ui/DriverTypes.java
+++
@@ -1,6 +0,0 @@
-package com.hostelworld.ui;
-
-public enum DriverTypes {
-    CHROME,
-    FIREFOX
-}
--- src/test/java/com/hostelworld/cucumber/pageobject/FactorialCalculatorPage.java
+++
@@ -1,9 +0,0 @@
-package com.hostelworld.cucumber.pageobject;
-
-import org.openqa.selenium.WebDriver;
-
-public class FactorialCalculatorPage extends PageObjectFactory {
-    public FactorialCalculatorPage(WebDriver driver) {
-        super(driver);
-    }
-}
