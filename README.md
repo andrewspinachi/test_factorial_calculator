221d10e4042c53d77b0e1c0172657c76eac80048
andrewspinachi <102956173+andrewspinachi@users.noreply.github.com>
2022-04-05 12:23:45 +0300

    Minor adjustments on SmokeFactorialTest

---
+++ README.md
@@ -0,0 +1,3 @@
+### test_factorial_calculator - rest automated scripts
+
+### Path to reported bugs: src/test/resources/bugreport/reported_bugs
--- src/test/java/com/hostelworld/tests/SmokeFactorialTest.java
+++ src/test/java/com/hostelworld/tests/SmokeFactorialTest.java
@@ -10,11 +10,8 @@ import static io.restassured.http.Method.GET;
 import static io.restassured.http.Method.POST;
 import static org.hamcrest.Matchers.equalTo;
 
-public class SmokeFactorialTest {
+public class SmokeFactorialTest extends BaseFactorialTest {
 
-
-    private static final String BASE_URI = "http://qainterview.pythonanywhere.com/factorial";
-    private static final String CONTENT_TYPE = "application/x-www-form-urlencoded; charset=UTF-8";
     private static final String REQUEST_BODY = "number=5";
 
     @Test
@@ -24,7 +21,7 @@ public class SmokeFactorialTest {
         logger.info("Start GET factorial calculator test!");
 
         when()
-                .request(GET, BASE_URI)
+                .request(GET, BASE_ENDPOINT)
                 .then()
                 .statusCode(200);
 
@@ -38,7 +35,7 @@ public class SmokeFactorialTest {
 
         with().body(REQUEST_BODY).contentType(CONTENT_TYPE)
                 .when()
-                .request(POST, BASE_URI)
+                .request(POST, BASE_ENDPOINT)
                 .then()
                 .statusCode(200)
                 .and()
